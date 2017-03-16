493e8bb629db37f4234970ae4b4a5512b0394e54
Ilya Bakulin <Ilya@Bakulin.de>
2017-03-16 02:04:47 +0100

    Implement setting MMCHS-specific config bit for 8-bit operating mode.

--- sys/arm/ti/ti_sdhci.c
+++ sys/arm/ti/ti_sdhci.c
@@ -250,6 +250,22 @@ ti_sdhci_write_1(device_t dev, struct sdhci_slot *slot, bus_size_t off,
 	struct ti_sdhci_softc *sc = device_get_softc(dev);
 	uint32_t val32;
 
+#ifdef MMCCAM
+	uint32_t newval32;
+	if (off == SDHCI_HOST_CONTROL) {
+		val32 = ti_mmchs_read_4(sc, MMCHS_CON);
+		newval32  = val32;
+		if (val & SDHCI_CTRL_8BITBUS) {
+			device_printf(dev, "!!!!!!!!!!!!!!!!!!1Enabling 8-bit bus!");
+			newval32 |= MMCHS_CON_DW8;
+		} else {
+			device_printf(dev, "!!!!!!!!!!!!!!!!!!1Disabling 8-bit bus!");
+			newval32 &= ~MMCHS_CON_DW8;
+		}
+		if (newval32 != val32)
+			ti_mmchs_write_4(sc, MMCHS_CON, newval32);
+	}
+#endif
 	val32 = RD4(sc, off & ~3);
 	val32 &= ~(0xff << (off & 3) * 8);
 	val32 |= (val << (off & 3) * 8);
